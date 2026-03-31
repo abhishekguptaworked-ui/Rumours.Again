2aca25129d4258492d29c0e291548f7b7b168a0c
Abhishek Gupta <abhishekguptaworked@gmail.com>
2026-03-31 11:31:22 +0530

    Whats.Popping
    🚀 Turning coffee ☕ into Python code 🐍  
    📊 Future Data Analyst in making  
    💻 Solving problems daily & leveling up

---
+++ abhishek.py
@@ -0,0 +1,146 @@
+# name = input("User: ")
+# location = input("Location: ")
+# roll_no = input("Roll no : ")
+# age = int(input("age: "))
+
+# if age >= 18 :
+#      print("Senior")
+# else :
+#      print("Junior")
+
+# traffic = input("Light : ")
+
+# if traffic == ("RED"):
+#      print("Ruk jaa Bsdk")
+# elif traffic == ("Green"):
+#      print("Chal Nikal Bsdk")
+# elif traffic == ("orange"):
+#      print("jada jaldi hai")
+# else : 
+#      print("nhi smhj aa rha hai green hone tak wait kar")
+
+
+# Marks = int(input("Marks : "))
+
+# if Marks >= 90 :
+#      print("A+")
+# elif Marks >= 80 and Marks < 90:
+#      print("B+")
+# elif Marks >=70 and Marks < 80:
+#      print("B")
+# elif Marks >=60 and Marks < 70:
+#      print("C")
+# elif Marks >= 50 and Marks <60:
+#      print("D")
+# else : 
+#      print("Fail")
+
+# A = int(input("A : "))
+# G = input("M/F : ")
+
+# if A  == 1 or A ==2 and G == ("F"):
+#     print("Fess is 100")
+# elif A == 3 or A == 4 or A == 5 and G == ("M"):
+#     print("Fess is 500")
+# else : 
+#     print("Not Vaild")
+
+
+# food  = input("Food : ")
+# eat = "yes" if food  == "cake" else "No"
+# print(eat)
+
+# food = input("Name : ")
+# print("Good") if food == "apple" or food == "organge" else print("not good so")
+
+# Age  = int(input("Age Pls : "))
+# vote = ("yes","no") [Age < 18]  
+# print(vote)
+
+# salary = float(input("salary : "))
+# tax = salary*(0.2,0.1) [salary <= 50000]
+# total  = salary - tax
+# print("Tax:", tax)
+# print("Total Salary after tax:",total)
+
+# p =  float(input("Principel:",  ))
+# r = float(input("Rate:",   ))
+# t = float(input("time:",  ))
+# si = (p*r*t/100)
+# print("simple interset:",si)
+
+# Arithmetic operator
+# a = 4
+# b = 6
+# print(a + b)
+# print(a - b)
+# print(a / b) 
+# print(a * b)
+# print(a ** b)
+# print(a % b)
+
+
+# relational operators
+# a = 64
+# b = 33
+# print(a == b)
+# print(a != b)
+# print(a <= b)
+
+# Assement operators
+
+# abhishek = 90
+# abhishek %= 2
+# print(abhishek)
+
+
+# # logical operators 
+# num1 = 10
+# num2 = 23.88
+# conversion
+# sum = (num1+num2)
+# print(sum)
+# print(type(sum))
+
+
+# NAME = input("NAME PUT ON : ")
+# print(type(NAME))
+# print(NAME)
+
+# name = input("your name : ")
+# age  = int(input("your age: "))
+# marks = float(input("your markhs : "))
+
+# print("welcome:", type(name))
+# print("its your age:", type(age))
+# print("its your marks:",type(marks))
+
+# num  = int(input("please enter your num :"))
+
+# if num  >= 90 : 
+#     print("A+")
+# elif num >= 89 and num < 80:
+#     print("B+")
+# elif num >= 79 and num < 70:
+#     print("C+")
+# elif num >= 69 and num < 60:
+#     print("D+")
+# elif num >= 59 and num < 50:
+#     print("E+")
+# else :
+#     print("Fail")
+
+
+# salary = int(input("enter your salary amount  : "))
+# gst = 0.18
+# pf = salary*(0.2,0.1) [salary <= 21000]
+# gst = salary*(0.18,0) [salary <= 100000]
+# total = salary - (gst+pf)
+# print("PF Amount:",pf)
+# print("Gst Amount:",gst )
+# print("After Dedicaiton Total Salary :", total)
+
+# num2 = float(input("enter your frist num :" ))
+# num1 = float(input("enter your second num :" ))
+# print("average =",(num1+num2) /2)
+
